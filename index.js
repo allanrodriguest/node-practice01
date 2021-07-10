8c72c205d866fb91d11d6ac908dab2f32a000251
Allan Rodrigues <allanrodriguest@hotmail.com>
2021-07-10 10:42:02 -0300

    chore: Initial Commit

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require("express");
+
+const server = express();
+
+server.use(express.json());
+
+const projetos = [];
+
+server.post("/projects", (req, res) => {
+  const { id } = req.body;
+  const { title } = req.body;
+
+  const project = {
+    id,
+    title,
+    tasks: [],
+  };
+
+  projetos.push(project);
+
+  return res.json(projetos);
+});
+
+server.get("/projects", (req, res) => {
+  return res.json(projetos);
+});
+
+server.put("/projects/:id", (req, res) => {
+  const { id } = req.params;
+
+  const novoProjeto = req.body;
+
+  projetos[id] = novoProjeto;
+
+  return res.json(projetos);
+});
+
+server.delete("/projects/:id", (req, res) => {
+  const { id } = req.params;
+
+  projetos.splice(id, 1);
+
+  return res.json(projetos);
+});
+
+server.post("/projects/:id/tasks", (req, res) => {
+  const { id } = req.params;
+
+  const { title } = req.body;
+  console.log("estou aqui");
+
+  const project = projetos.find((p) => p.id == id);
+
+  project.tasks.push(title);
+
+  return res.json(projetos[id]);
+});
+
+server.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
